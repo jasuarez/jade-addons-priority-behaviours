101a377fe52ae247f315833bb28fe993a013492e
Juan A. Suarez Romero <jsuarezr@udc.es>
2006-10-13 15:37:13 +0000

    Metemos los tests de funcionalidad que nos faltaban.
    git-svn-id: file:///var/svn/repos/intrusos/jade_add-ons/priority-behaviours/trunk@51 f84ad317-7ff1-0310-a2aa-b5c2ac10b754

---
+++ jadeTestSuite/test/parallelprioritybehaviour/common/BehaviourExampleChange.java
@@ -0,0 +1,67 @@
+/*****************************************************************
+ JADE - Java Agent DEvelopment Framework is a framework to develop multi-agent
+ systems in compliance with the FIPA specifications.
+ Copyright (C) 2000 CSELT S.p.A. 
+ 
+ GNU Lesser General Public License
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation, 
+ version 2.1 of the License. 
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the
+ Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ Boston, MA  02111-1307, USA.
+ *****************************************************************/
+
+/*****************************************************************
+ Laboratory for Research and Development in Artificial Intelligence (LIDIA)
+ Computer Science Department
+ University of A Coruna, Spain
+ 
+ $Id: BehaviourExample.java 48 2006-10-10 14:28:12Z jota $
+ *****************************************************************/
+
+package test.parallelprioritybehaviour.common;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.ParallelPriorityBehaviour;
+
+public class BehaviourExampleChange extends Behaviour {
+    private int counter = 3;
+    private int id;
+    private ParallelPriorityBehaviourForTest parent;
+    private boolean changeNow;
+    
+    public BehaviourExampleChange() {
+        super();
+    }
+    
+    public BehaviourExampleChange(ParallelPriorityBehaviourForTest parent, int id, boolean changeNow) {
+        super();
+        this.parent = parent;
+        this.id = id;
+        this.changeNow = changeNow;
+     }
+
+    public void action() {
+        //System.out.print(id+"-");
+        parent.ranBehaviour(id);
+        counter--;
+        if (counter==2) {
+            ParallelPriorityBehaviour parent = (ParallelPriorityBehaviour) getParent();
+            parent.changePriority(this, 0, changeNow);
+        }
+    }
+    
+    public boolean done() {
+        return counter==0;
+    }
+}
---
+++ jadeTestSuite/test/parallelprioritybehaviour/tests/Test3.java
@@ -0,0 +1,50 @@
+/*
+ * Created on 12-oct-2006
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * $Id$
+ */
+
+
+package test.parallelprioritybehaviour.tests;
+
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.SequentialBehaviour;
+import test.common.Test;
+import test.parallelprioritybehaviour.common.ParallelPriorityBehaviourForTest;
+
+public class Test3 extends Test {
+    class ValidateBehaviour extends OneShotBehaviour {
+        ParallelPriorityBehaviourForTest toValidate;
+        int[] expectedRun = {3,3,3,2,1,2,2,1,1};
+        
+        public ValidateBehaviour(ParallelPriorityBehaviourForTest ppbft) {
+            this.toValidate = ppbft;
+        }
+        
+        public void action() {
+            int[] currentRun = toValidate.getCurrentRun();
+            for (int i=0; i < expectedRun.length; i++)
+                if (expectedRun[i]!=currentRun[i]) {
+                    failed("Expected child "+expectedRun[i]+", but was "+currentRun[i]);
+                    return;
+                }
+            passed("Test passed");
+        }
+    }
+    
+    public Behaviour load(Agent a) {
+        SequentialBehaviour sb = new SequentialBehaviour(a);
+        ParallelPriorityBehaviourForTest ppbft = new ParallelPriorityBehaviourForTest();
+        ppbft.createBehaviour(1, 2);
+        ppbft.createBehaviour(2, 1);
+        ppbft.createBehaviour(3, 0);
+        sb.addSubBehaviour(ppbft);
+        sb.addSubBehaviour(new ValidateBehaviour(ppbft));
+        return sb;
+    }
+}
