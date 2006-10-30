2375d0189af2d6a1f61ad9a3c6ca7ab97d4d6f1a
Juan A. Suarez Romero <jsuarezr@udc.es>
2006-10-30 10:30:21 +0000

    Creamos un tutorial y la documentacion adecuada para el uso del add-on.
    git-svn-id: file:///var/svn/repos/intrusos/jade_add-ons/priority-behaviours/trunk@53 f84ad317-7ff1-0310-a2aa-b5c2ac10b754

---
+++ tutorial/tutorial/images.pl
@@ -0,0 +1,219 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/langleA^4,:underline{D^1}rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="67" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img30.png"
+ ALT="$\langle A^4,\:\underline{D^1}\rangle$">|; 
+
+$key = q/langleA_3^4,:B_1^1,:D_0^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="$\langle A_3^4,\:B_1^1,\:D_0^1\rangle$">|; 
+
+$key = q/langleA_2^4rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="36" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img19.png"
+ ALT="$\langle A_2^4\rangle$">|; 
+
+$key = q/langleA_3^4,:underline{B_0^1},:D_0^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="$\langle A_3^4,\:\underline{B_0^1},\:D_0^1\rangle$">|; 
+
+$key = q/langleA^4,:B^1,:underline{C^0},:D^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="128" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img25.png"
+ ALT="$\langle A^4,\:B^1,\:\underline{C^0},\:D^1\rangle$">|; 
+
+$key = q/langleA^4,:B^1,:D^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img26.png"
+ ALT="$\langle A^4,\:B^1,\:D^1\rangle$">|; 
+
+$key = q/langleA_4^4rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="36" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img22.png"
+ ALT="$\langle A_4^4\rangle$">|; 
+
+$key = q/B;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$B$">|; 
+
+$key = q/langlerangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img23.png"
+ ALT="$\langle \rangle$">|; 
+
+$key = q/D;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="18" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$D$">|; 
+
+$key = q/C;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$C$">|; 
+
+$key = q/langleA_2^4,:underline{B_0^1},:D_0^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="$\langle A_2^4,\:\underline{B_0^1},\:D_0^1\rangle$">|; 
+
+$key = q/{lstlisting}[caption={Sourcecodeforchildren},label=code:children]importjade.coreid+"->");counter--;}{par{publicbooleandone(){returncounter==0;}}{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="571" HEIGHT="415" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="\begin{lstlisting}[caption={Source code for children}, label=code:children]
+impo...
+...ter-;
+}
+\par
+public boolean done() {
+return counter==0;
+}
+}
+\end{lstlisting}">|; 
+
+$key = q/langleA_4^4,:B_1^1,:C_0^0,:D_1^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="128" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$\langle A_4^4,\:B_1^1,\:C_0^0,\:D_1^1\rangle$">|; 
+
+$key = q/{lstlisting}[caption={Sourcecodefortheagentusingtexttt{ParallelPriorityBehaviourhaviour("C"),0);ppb.addSubBehaviour(newChildBehaviour("D"),1);}}{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="571" HEIGHT="300" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="\begin{lstlisting}[caption={Source code for the agent using
+\texttt{ParallelPrio...
+...), 0);
+ppb.addSubBehaviour(new ChildBehaviour(''D''), 1);
+}
+}
+\end{lstlisting}">|; 
+
+$key = q/langleA^4,rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="43" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img31.png"
+ ALT="$\langle A^4,\rangle$">|; 
+
+$key = q/langleA_2^4,:underline{D_0^1}rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="67" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img18.png"
+ ALT="$\langle A_2^4,\:\underline{D_0^1}\rangle$">|; 
+
+$key = q/{lstlisting}[caption={Sourcecodefortheagentusingtexttt{SequentialPriorityBehaviohaviour("C"),0);spb.addSubBehaviour(newChildBehaviour("D"),1);}}{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="571" HEIGHT="300" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img24.png"
+ ALT="\begin{lstlisting}[caption={Source code for the agent using
+\texttt{SequentialPr...
+...), 0);
+spb.addSubBehaviour(new ChildBehaviour(''D''), 1);
+}
+}
+\end{lstlisting}">|; 
+
+$key = q/langleA_2^4,:B_0^1,:D_0^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="$\langle A_2^4,\:B_0^1,\:D_0^1\rangle$">|; 
+
+$key = q/langleA_2^4,:D_0^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="67" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img17.png"
+ ALT="$\langle A_2^4,\:D_0^1\rangle$">|; 
+
+$key = q/A;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$A$">|; 
+
+$key = q/langleA_3^4,:B_1^1,:underline{D_0^1}rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="$\langle A_3^4,\:B_1^1,\:\underline{D_0^1}\rangle$">|; 
+
+$key = q/langleA_4^4,:B_1^1,:D_1^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$\langle A_4^4,\:B_1^1,\:D_1^1\rangle$">|; 
+
+$key = q/langleA^4,:underline{B^1},:D^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img28.png"
+ ALT="$\langle A^4,\:\underline{B^1},\:D^1\rangle$">|; 
+
+$key = q/langleA_4^4,:B_1^1,:underline{C_0^0},:D_1^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="128" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$\langle A_4^4,\:B_1^1,\:\underline{C_0^0},\:D_1^1\rangle$">|; 
+
+$key = q/langleA_0^4rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="36" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img20.png"
+ ALT="$\langle A_0^4\rangle$">|; 
+
+$key = q/langleA_3^4,:B_0^1,:D_0^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$\langle A_3^4,\:B_0^1,\:D_0^1\rangle$">|; 
+
+$key = q/langleA^4,:B^1,:underline{D^1}rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img27.png"
+ ALT="$\langle A^4,\:B^1,\:\underline{D^1}\rangle$">|; 
+
+$key = q/langleunderline{A^4}rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="36" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img32.png"
+ ALT="$\langle \underline{A^4}\rangle$">|; 
+
+$key = q/langleunderline{A_0^4}rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="36" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img21.png"
+ ALT="$\langle \underline{A_0^4}\rangle$">|; 
+
+$key = q/langleA^4,:D^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="67" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img29.png"
+ ALT="$\langle A^4,\:D^1\rangle$">|; 
+
+$key = q/langleA_3^4,:B_1^1,:D_1^1rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="98" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="$\langle A_3^4,\:B_1^1,\:D_1^1\rangle$">|; 
+
+1;
+
---
+++ tutorial/tutorial/labels.pl
@@ -0,0 +1,41 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/table:parallelprioritybehaviour/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:sequentialprioritybehaviour/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/table:parallelprioritybehaviour/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/code:children/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/code:sequentialagent/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/code:agentparallel/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:sequentialprioritybehaviour/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+1;
+
